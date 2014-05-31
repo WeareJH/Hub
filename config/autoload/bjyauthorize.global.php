1103448fe5d08d87fe0e097457ae144feb63fd21
Aydin Hassan <aydin@hotmail.co.uk>
2014-05-31 20:29:25 +0100

    Short array syntax

--- config/autoload/bjyauthorize.global.php
+++ config/autoload/bjyauthorize.global.php
@@ -1,6 +1,6 @@
 <?php
-return array(
-    'bjyauthorize' => array(
+return [
+    'bjyauthorize' => [
 
         // set the 'guest' role as default (must be defined in a role provider)
         'default_role' => 'guest',
@@ -29,54 +29,48 @@ return array(
          *
          * Consider enabling either the controller or the route guard depending on your needs.
          */
-        'guards' => array(
+        'guards' => [
 
             /* If this guard is specified here (i.e. it is enabled), it will block
              * access to all routes unless they are specified here.
              */
-            'BjyAuthorize\Guard\Route' => array(
+            'BjyAuthorize\Guard\Route' => [
 
                 //user routes
-                array('route' => 'zfcuser/register',                            'roles' => array('guest')),
-                array('route' => 'zfcuser/login',                               'roles' => array('guest')),
-                array('route' => 'zfcuser',                                     'roles' => array('user')),
-                array('route' => 'zfcuser/changeemail',                         'roles' => array('user')),
-                array('route' => 'zfcuser/changepassword',                      'roles' => array('user')),
-                array('route' => 'zfcuser/logout',                              'roles' => array('user')),
+                ['route' => 'zfcuser/register',                             'roles' => ['guest']],
+                ['route' => 'zfcuser/login',                                'roles' => ['guest']],
+                ['route' => 'zfcuser',                                      'roles' => ['user']],
+                ['route' => 'zfcuser/changeemail',                          'roles' => ['user']],
+                ['route' => 'zfcuser/changepassword',                       'roles' => ['user']],
+                ['route' => 'zfcuser/logout',                               'roles' => ['user']],
 
                 //social auth routes
-                array('route' => 'scn-social-auth-user/login',                  'roles' => array('guest')),
-                array('route' => 'scn-social-auth-user/login/provider',         'roles' => array('guest')),
-                array('route' => 'scn-social-auth-hauth',                       'roles' => array('guest')),
-                array('route' => 'scn-social-auth-user/authenticate/provider',  'roles' => array('guest')),
-                array('route' => 'scn-social-auth-user',                        'roles' => array('user')),
-                array('route' => 'scn-social-auth-user/logout',                 'roles' => array('user')),
+                ['route' => 'scn-social-auth-user/login',                   'roles' => ['guest']],
+                ['route' => 'scn-social-auth-user/login/provider',          'roles' => ['guest']],
+                ['route' => 'scn-social-auth-hauth',                        'roles' => ['guest']],
+                ['route' => 'scn-social-auth-user/authenticate/provider',   'roles' => ['guest']],
+                ['route' => 'scn-social-auth-user',                         'roles' => ['user']],
+                ['route' => 'scn-social-auth-user/logout',                  'roles' => ['user']],
 
                 //home
-                array('route' => 'home',                                        'roles' => array('user')),
+                ['route' => 'home',                                         'roles' => ['user']],
 
                 //flexitime
-                array('route' => 'flexi-time',                                  'roles' => array('user')),
-                array('route' => 'flexi-time-rest',                             'roles' => array('user')),
+                ['route' => 'flexi-time',                                   'roles' => ['user']],
+                ['route' => 'flexi-time-rest',                              'roles' => ['user']],
 
                 //flexitime console routes
-                array('route' => 're-calc-running-balance',                     'roles' => array('guest')),
-
+                ['route' => 're-calc-running-balance',                      'roles' => ['guest']],
 
                 //settings
-                array('route' => 'settings',                                    'roles' => array('user')),
-
-                //overtime
-                array('route' => 'overtime',                                    'roles' => array('user')),
+                ['route' => 'settings',                                     'roles' => ['user']],
 
                 //doctrine routes
-                array('route' => 'doctrine_cli',                                'roles' => array('guest')),
+                ['route' => 'doctrine_cli',                                 'roles' => ['guest']],
 
-
-                array('route' => 'zfcadmin/flexi-time',                                'roles' => array('admin')),
-
-            ),
-        ),
+                ['route' => 'zfcadmin/flexi-time',                          'roles' => ['admin']],
+            ],
+        ],
         'unauthorized_strategy' => 'BjyAuthorize\View\RedirectionStrategy',
-    ),
-);
+    ],
+];
